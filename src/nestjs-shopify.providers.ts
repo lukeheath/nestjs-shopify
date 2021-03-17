0b138622ba8ee454e901e475f12e6688f161f1ce
Luke Heath <luke@heath.software>
2021-03-17 13:39:58 -0500

    Debug

--- src/nestjs-shopify.providers.ts
+++ src/nestjs-shopify.providers.ts
@@ -5,6 +5,7 @@ import { NESTJS_SHOPIFY_OPTIONS } from './constants';
 export function createNestjsShopifyProviders(
   options: NestjsShopifyOptions,
 ) {
+  console.log('options: ', options);
   return [
     {
       provide: NESTJS_SHOPIFY_OPTIONS,
